f8cd3472954dd7583fec742dbbc2be78df032d3a
Pranav0826git <pranavk0826@gmail.com>
2026-03-16 21:14:33 +0530

    Add global error handling middleware

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function errorHandler(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).json({
+        message: 'Something went wrong!',
+        error: err.message
+    });
+}
+
+module.exports = errorHandler;
