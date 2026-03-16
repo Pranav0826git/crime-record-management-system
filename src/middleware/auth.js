7569cb05fcdb1f3706c4563d0cc4adb3ce7ed2b8
Pranav0826git <pranavk0826@gmail.com>
2026-03-16 21:14:44 +0530

    Create JWT authentication middleware for protecting routes

---
+++ src/middleware/auth.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1];
+
+    if (!token) {
+        return res.status(403).json({ message: 'No token provided!' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Failed to authenticate token.' });
+        }
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authMiddleware;
