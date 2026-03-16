2b1f14b9514884214d0478a3559b219dcdd2f39c
Pranav0826git <pranavk0826@gmail.com>
2026-03-16 21:12:52 +0530

    Create src/controllers/crimeController.js with CRUD operations

---
+++ src/controllers/crimeController.js
@@ -0,0 +1,28 @@
+// crimeController.js
+
+// Function to get crime records
+function getCrimeRecords(req, res) {
+    // Logic to retrieve crime records
+}
+
+// Function to create a new crime record
+function createCrimeRecord(req, res) {
+    // Logic to create a new crime record
+}
+
+// Function to update an existing crime record
+function updateCrimeRecord(req, res) {
+    // Logic to update a crime record
+}
+
+// Function to delete a crime record
+function deleteCrimeRecord(req, res) {
+    // Logic to delete a crime record
+}
+
+module.exports = {
+    getCrimeRecords,
+    createCrimeRecord,
+    updateCrimeRecord,
+    deleteCrimeRecord
+};
